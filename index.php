6ac75ff9d90e97c45d2cca668ae06d65c32bcd47
Mochamad Bachtiyar Eko Cahyo Putro <bachtiar.cahyoo@gmail.com>
2018-07-20 16:26:35 +0700

    Fix some error

--- index.php
+++ index.php
@@ -99,7 +99,7 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                 //buat template message untuk menampung banyak message type
                   //$multiMessageBuilder = new MultiMessageBuilder();
                   //pesan pertama
-                  $imageUrl = 'https://i.ytimg.com/vi/x3Ovjh7MUAY/maxresdefault.jpg';
+                  $imageUrl = 'https://meepwnd-health-assistant.herokuapp.com/static/rs-logo-1.png';
                   $buttonTemplateBuilder = new ButtonTemplateBuilder(
                     'Rumah Sakit Universitas Brawijaya',
                     'Jl. Soekarno - Hatta, Lowokwaru, Kec. Lowokwaru, Kota Malang, Jawa Timur 65141',
