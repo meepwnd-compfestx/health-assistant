f111a1fb438ba00046d9ce2594b95d2f94f05301
Mochamad Bachtiyar Eko Cahyo Putro <bachtiar.cahyoo@gmail.com>
2018-07-21 13:43:59 +0700

    Find error #10

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $dotenv = new Dotenv(__DIR__);
 $dotenv->load();
 
 include('functional.php');
-include('db.php');
+//include('db.php');
 
 // set LINE channel_access_token and channel_secret
 $channel_access_token = $_ENV['CHANNEL_ACCESS_TOKEN'];
@@ -32,6 +32,27 @@ $channel_secret = $_ENV['CHANNEL_SECRET'];
 $httpClient = new CurlHTTPClient($channel_access_token);
 $bot = new LINEBot($httpClient, ['channelSecret' => $channel_secret]);
 
+//db
+
+try{
+//Set DSN data source name
+  $db = array('host'=>$_ENV['DB_HOST'], 'dbname'=>$_ENV['DB_NAME'], 'user'=>$_ENV['DB_USERNAME'], 'password'=>$_ENV['DB_PASSWORD'], 'port'=>5432);
+  $host = $db['host'];
+  $port = $db['port'];
+  $dbname = $db['dbname'];
+  $user = $db['user'];
+  $password = $db['password'];
+
+  $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password;";
+//create a pdo instance
+  $pdo = new PDO($dsn, $user, $password);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+  echo 'Connection failed: ' . $e->getMessage();
+}
 
 $configs =  [
     'settings' => ['displayErrorDetails' => true],
