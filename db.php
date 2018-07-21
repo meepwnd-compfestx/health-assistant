e0ed6a492117ed9011f3d30000e8e8dcbec8bb95
Mochamad Bachtiyar Eko Cahyo Putro <bachtiar.cahyoo@gmail.com>
2018-07-21 16:18:56 +0700

    Find error #18

--- db.php
+++ db.php
@@ -5,7 +5,18 @@ use \Dotenv\Dotenv;
 $dotenv = new Dotenv(__DIR__);
 $dotenv->load();
 
-try{
+$db = array('host'=>$_ENV['DB_HOST'], 'dbname'=>$_ENV['DB_NAME'], 'user'=>$_ENV['DB_USERNAME'], 'password'=>$_ENV['DB_PASSWORD'], 'port'=>5432);
+
+$host = $db['host'];
+$port = $db['port'];
+$dbname = $db['dbname'];
+$user = $db['user'];
+$password = $db['password'];
+
+$conn = pg_connect($host." ".$port." ".$dbname." ".$user." ".$password);
+
+echo pg_last_error($conn);
+/*try{
 //Set DSN data source name
   $db = array('host'=>$_ENV['DB_HOST'], 'dbname'=>$_ENV['DB_NAME'], 'user'=>$_ENV['DB_USERNAME'], 'password'=>$_ENV['DB_PASSWORD'], 'port'=>5432);
   $host = $db['host'];
@@ -20,8 +31,9 @@ try{
   $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
   $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 }
 catch (PDOException $e) {
   echo 'Connection failed: ' . $e->getMessage();
-}
+}*/
 ?>
