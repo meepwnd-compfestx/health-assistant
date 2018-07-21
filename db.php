c12209e388548c2f1db0df90c0906ce6ddb6398a
Mochamad Bachtiyar Eko Cahyo Putro <bachtiar.cahyoo@gmail.com>
2018-07-21 12:06:22 +0700

    Connected to db

--- db.php
+++ db.php
@@ -2,14 +2,13 @@
 try{
 //Set DSN data source name
   $db = array('host'=>getenv('DB_HOST'), 'dbname'=>getenv('DB_NAME'), 'user'=>getenv('DB_USERNAME'), 'password'=>getenv('DB_PASSWORD'), 'port'=>5432);
-  
   $host = $db['host'];
   $port = $db['port'];
   $dbname = $db['dbname'];
   $user = $db['user'];
   $password = $db['password'];
 
-  $dsn = "pgsql:host=$host;dbname=$dbname;user=$user;password=$password;port=$port;sslmode=require;";
+  $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password;sslmode=require;";
 //create a pdo instance
   $pdo = new PDO($dsn, $db['user'], $db['password']);
   $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
